a30a2a20e7e3e301da6c708ffa0206c9fe3f7e32
Elvin Lai <61441620+elvinlai1@users.noreply.github.com>
2021-12-08 23:58:14 -0800

    updaetd  print_session.php

--- auth/print_session.php
+++ auth/print_session.php
@@ -2,22 +2,7 @@
 session_start();
 print("SESSION:  ");
 echo "<pre>";
-print("login: ");
-print_r($_SESSION['login']);
-print("\nuserID: ");
-print_r($_SESSION['userID']);
-print("\nuserName: ");
-print_r($_SESSION['userName']);
-print("\nemail: ");
-print_r($_SESSION['email']);
-print("\nphone: ");
-print_r($_SESSION['phone']);
-print("\naddress: ");
-print_r($_SESSION['address']);
-print("\npostalCode: ");
-print_r($_SESSION['postalCode']);
-print("\ncity: ");
-print_r($_SESSION['city']);
-var_dump($_POST);
+var_dump($_SESSION);
+print("POST: ");
 echo "</pre>";
 ?>
